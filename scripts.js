6bd706b34670e7edd68402e2f7e7f6b657ba29ce
Hannah Bare <johnd>
2018-01-31 18:22:08 -0700

    Refactor level 3
    Rearrange min and max input fields in order for the random number to be generated only once per game. Add button for min and max fields as well in order to have a specific event listener

--- scripts.js
+++ scripts.js
@@ -1,75 +1,130 @@
+//THINGS I WANT TO MAKE BETTER
+//1. less global variables
+//2. understand how to do second level of phase 3.
+//3. use a switch statment instead of if/else statements
+
+
+
 var userInputMin = document.querySelector('#userinput__low');
 var userInputMax = document.querySelector('#userinput__high');
-var userInputBtn = document.querySelector('#userinput__submit');
-var guessBtn = document.getElementById('guess__btn');
-var clearBtn = document.getElementById('clear__btn');
-var resetBtn = document.getElementById('reset__btn');
+var userGuess = document.querySelector('#userinput__guess');
+var setRangeBtn = document.querySelector('#range__btn');
+var guessBtn = document.querySelector('#guess__btn');
+var clearBtn = document.querySelector('#clear__btn');
+var resetBtn = document.querySelector('#reset__btn');
+var minNum = 0;
+var maxNum = 100;
+var randomNum;
 
-userInputBtn.addEventListener('click', generateRandomNum);
-// guessBtn.addEventListener('click', showValue);
-// userInputMin.addEventListener('keyup', enableBtns);
-// clearBtn.addEventListener('click', clearInputField);
-// resetBtn.addEventListener('click', resetGame);
+guessBtn.addEventListener('click', showValue);
+setRangeBtn.addEventListener('click', genNumber);
+userGuess.addEventListener('keyup', enableBtns);
+clearBtn.addEventListener('click', clearInputField);
+resetBtn.addEventListener('click', resetGame);
 
-function generateRandomNum(){
-  var userMinNum = userInputMin.value;
-  var minNum = parseInt(userMinNum);
-  var userMaxNum = userInputMax.value;
-  var maxNum = parseInt(userMaxNum);
-  var randomNum = Math.floor(Math.random()* (maxNum - minNum) + minNum)+1;
-  console.log(randomNum);
+function genNumber(){
+  minNumber();
+  maxNumber();
+  randomNumber();
+}
+
+function minNumber(){
+  minNum = parseInt(userInputMin.value);
+}
+
+function maxNumber(){
+  maxNum = parseInt(userInputMax.value);
+}
+
+function randomNumber(){
+  randomNum = Math.floor(Math.random() * (maxNum - minNum) + minNum) + 1;
+  console.log('Watta cheata: ' + randomNum);
+}
+
+function showValue(event){
+  event.preventDefault();
+  var guessVal = getValue();
+  compareVals(guessVal);
+  var displayNum = document.getElementById('guess--display');
+  displayNum.innerHTML = guessVal;
+}
+
+function getValue(){
+  var userNum = userGuess.value; 
+  var guessedNum = parseInt(userNum);
+  return guessedNum;
 }
 
 
-// function showValue(e){
-//   e.preventDefault();
-//   var value = getValue();
-//   compareVals(value);
-//   var guessDisplay = document.getElementById('guess--display');
-//   guessDisplay.innerHTML = value;
-// }
+function compareVals(guessVal){
+  var compareResults = document.getElementById('compare--results');
+  var result; 
+  compareResults.innerText = result;
+  switch(guessVal){ 
+    case guessVal > maxNum:
+      result = "That's too high! Pick a number between " + minNum + " and " + maxNum;
+      break;
+    case guessVal < minNum:
+      result = "That's too low! Pick a number between " + minNum + " and " + maxNum;
+      break;
+    case Number.isNaN(guessVal):
+      result = "ERROR: That isn't a number. Pick a number between " + minNum + " and " + maxNum;
+      break;
+    case guessVal < randomNum:
+      result = "That's too low!";
+      break;
+    case guessVal > randomNum:
+      result = "That's too high!";
+      break;
+    case guessVal === randomNum:
+      result = "BOOM";
+      levelUp();
+      break;
+    }
+}
 
-// function getValue(){
-//   var stringValue = userInputMin.value;
-//   var parsedValue = parseInt(stringValue);
-//   return parsedValue;
-// }
-
-// function compareVals(value){
+// function compareVals(guessVal){
 //   var compareResults = document.getElementById('compare--results');
 //   var result; 
-//   if (value > 100){
-//     result = "ERROR: That's out of range. <br/>Pick a number between 1 and 100."
-//   } else if (value < 1){
-//     result = "ERROR:: That's out of range. <br/>Pick a number between 1 and 100."
-//   } else if (Number.isNaN(value)){
-//     result = "ERROR: That isn't a number. <br/>Pick a number between 1 and 100."
-//   } else if (value < randomNum){
+//   if (guessVal > maxNum){
+//     result = "That's too high! Pick a number between " + minNum + " and " + maxNum;
+//   } else if (guessVal < minNum){
+//     result = "That's too low! Pick a number between " + minNum + " and " + maxNum;
+//   } else if (Number.isNaN(guessVal)){
+//     result = "ERROR: That isn't a number. Pick a number between " + minNum + " and " + maxNum;
+//   } else if (guessVal < randomNum){
 //     result = "That's too low!"
-//   } else if (value > randomNum){
+//   } else if (guessVal > randomNum){
 //     result = "That's too high!"
-//   } else if (value === randomNum){
+//   } else if (guessVal === randomNum){
 //     result = "BOOM";
-//   } compareResults.innerHTML = result;
+//     levelUp();
+//   } compareResults.innerText = result;
 // }
 
-// function enableBtns(value){
-//   if( value != ''){
-//     guessBtn.disabled = false;
-//     clearBtn.disabled = false;
-//     resetBtn.disabled = false;
-//   }
-// }
+function levelUp(){
+  var levelUpInfo = document.querySelector('.hide');
+  var tenLower = document.querySelector('.ten-lower');
+  var tenHigher = document.querySelector('.ten-higher');
+  minNum = minNum - 10;
+  maxNum = maxNum + 10;
+  randomNumber();
+  levelUpInfo.classList.remove('hide');
+  levelUpInfo.innerText = ('Nice work! Your range is now ' + minNum + ' through ' + maxNum);
+}
 
-// function clearInputField(){
-//   userInput.value = ' ';
-// }
+function enableBtns(value){
+  if( value != ''){
+    guessBtn.disabled = false;
+    clearBtn.disabled = false;
+    resetBtn.disabled = false;
+  }
+}
 
-// function resetGame(){
-//   window.location.reload(true);
-// }
+function clearInputField(){
+  userInput.value = ' ';
+}
 
-
-
-
-
+function resetGame(){
+  window.location.reload(true);
+}
